092a65fe0a8ba2cab945a867f2efcffffdabfc4b
abdullahmashhadi <abdullah6blue@gmail.com>
2024-12-15 03:37:32 +0500

    created board in pygame

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-def CreateGraphicalBoard():
-    for row in range (8):
-        for column in range(8):
-            is_light_square=((column+row)%2)==0
-            square_colour= "light" if is_light_square else "dark"
-            print(f"grid position {row, column} is: {square_colour}")
+import pygame
+
+#setting up the pygame display
+pygame.init()
+WIDTH, HEIGHT = 800, 800
+SQUARE_WIDTH = WIDTH // 8
+SQUARE_HEIGHT= HEIGHT//8
+WHITE_COLOR = (255, 255, 255)
+BLACK_COLOR = (0, 0, 0)
+screen=pygame.display.set_mode((WIDTH,HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Chess Board")
+
+#creating board
+def CreateBoard():
+    for rank in range (7,-1,-1):
+        for file in range(8):
+            is_white_square=((rank+file)%2)!=0
+            square_color= WHITE_COLOR if is_white_square else BLACK_COLOR
+            pygame.draw.rect(
+                screen,
+                square_color,
+                pygame.Rect(file * SQUARE_WIDTH, (7-rank) * SQUARE_HEIGHT, SQUARE_WIDTH, SQUARE_HEIGHT)
+            )
             
     
-CreateGraphicalBoard()
 
+def main():
+    running=True
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+        CreateBoard()
+        pygame.display.flip()
+
+    pygame.quit()
+    
+
+main()
